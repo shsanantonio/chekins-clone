29ea5a90c2ab47a54b5637f8ff2908b396da36ae
shsanantonio <hi@sheleen.dev>
2023-10-09 10:05:24 -0700

    Added Benefits Section

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import SearchForm from '@/components/SearchForm';
 import Tagline from '@/components/Tagline';
+import Benefits from '@/components/section/Benefits';
 
 export default function Home() {
   return (
     <div>
       <Tagline />
       {/* <SearchForm /> */}
+      <Benefits />
     </div>
   );
 }
