2f746ca8c489472de9cb63de82754f0ada00bef9
shsanantonio <hi@sheleen.dev>
2023-10-10 09:48:54 -0700

    Integrate SearchMap and Sort components with HoteListing page

--- app/hotel-listing/page.jsx
+++ app/hotel-listing/page.jsx
@@ -1,6 +1,8 @@
 'use client';
 
 import SearchForm from '@/components/ui/SearchForm';
+import SearchMap from '@/components/ui/SearchMap';
+import Sort from '@/components/ui/Sort';
 
 const HotelListing = () => {
   return (
@@ -9,7 +11,8 @@ const HotelListing = () => {
       <div className="font-Montserrat container px-0 lg:px-3 xl:px-0 lg:w-full xl:w-11/12 flex flex-col mt-4 mx-auto">
         <div className="flex flex-row md:space-x-4 space-x-0 space-y-4 md:space-y-0">
           <div className="hidden  relative z-10  lg:flex flex-col w-3/12 space-y-2">
-            Filters
+            <SearchMap />
+            <Sort />
           </div>
           <div>Hotel List</div>
         </div>
