d89d1eef2ff920a5680b0c0ba24c8a1e6eb3c339
shsanantonio <hi@sheleen.dev>
2023-10-10 09:46:00 -0700

    Added SearchMap Component

---
+++ components/ui/SearchMap.jsx
@@ -0,0 +1,13 @@
+const SearchMap = () => {
+  return (
+    <div className="cursor-pointer ">
+      <img
+        src="/images/mapFIlter.335aed95af5e4c507183.png"
+        alt="mapFIlter"
+        className="w-full"
+      />
+    </div>
+  );
+};
+
+export default SearchMap;
