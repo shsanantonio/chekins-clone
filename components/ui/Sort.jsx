b0c9a8ca88ca862c7829146161b0639e6a413384
shsanantonio <hi@sheleen.dev>
2023-10-11 01:02:37 -0700

    Improve Accessibility More

--- components/ui/Sort.jsx
+++ components/ui/Sort.jsx
@@ -2,7 +2,9 @@ const Sort = () => {
   return (
     <div className="flex flex-col gap-3">
       <h2 className="font-bold text-[18px] mt-2">Sort By</h2>
-      <label htmlFor="sort-select visually-hidden">Sort By</label>
+      <label htmlFor="sort-select" className="visually-hidden">
+        Sort By
+      </label>
       <select
         className="border w-full md:w-auto h-10 border-gray-300 rounded-md px-2 py-2"
         id="sort-select"
